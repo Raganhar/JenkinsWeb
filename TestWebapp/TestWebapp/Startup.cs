cf27773bb084a9e415cf6be192d6153e02bee094
Sarothia <januspeis@gmail.com>
2015-10-01 09:53:21 +0200

    Initial commit

---
+++ TestWebapp/TestWebapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebapp.Startup))]
+namespace TestWebapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
